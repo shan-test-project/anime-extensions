b7943d07f7edd4586160d34a69565e135545bc5a
Shan <bosssun356@gmail.com>
2026-08-06 11:39:23 +0600

    fix: remove m3u8server dependency, bump baseVersionCode to 2

--- lib-multisrc/anikototheme/build.gradle.kts
+++ lib-multisrc/anikototheme/build.gradle.kts
@@ -4,9 +4,8 @@ plugins {
     alias(kei.plugins.multisrc)
 }
 
-baseVersionCode = 1
+baseVersionCode = 2
 
 dependencies {
     implementation(project(":lib:playlistutils"))
-    implementation(project(":lib:m3u8server"))
 }
